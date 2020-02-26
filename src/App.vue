a69a279475c22f45c5ac17e7b33a98284536a427
VernerBorn <vernerborn@gmail.com>
2020-02-26 22:45:05 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <HeaderApp head="Траспорт Ярославля" />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +9,36 @@
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import HeaderApp from "@/components/HeaderApp.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderApp
+  }
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: Neuron;
+  src: url("./assets/fonts/Neuron-DemiBold.woff2") format("woff2"),
+    url("./assets/fonts/Neuron-DemiBold.woff") format("woff");
+  font-style: normal;
+  font-weight: normal;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@font-face {
+  font-family: Neuron;
+  src: url("./assets/fonts/Neuron.woff2") format("woff2"),
+    url("./assets/fonts/Neuron.woff") format("woff");
+  font-style: normal;
+  font-weight: bold;
+}
+body {
+  font-size: 18px;
+  font-family: Neuron, sans-serif;
 }
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        <h1 class="header__head">{{ head }}</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderApp",
+  props: {
+    head: String
+  }
+};
+</script>
+
+<style lang="scss">
+.header {
+  height: 90px;
+  background-color: #fff;
+}
+</style>
